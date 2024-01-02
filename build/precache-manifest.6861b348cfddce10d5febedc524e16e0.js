5ba5d3a0b2b747623228e0f923e3e6d86093567b
Carlosff02 <carlosfelipefdezfalcon@gmail.com>
2024-01-02 10:16:50 -0500

    error solved

---
+++ build/precache-manifest.6861b348cfddce10d5febedc524e16e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f35db586f6347d16da6",
+    "url": "/CarlosFernandez/build/static/css/main.992558c9.chunk.css"
+  },
+  {
+    "revision": "0f35db586f6347d16da6",
+    "url": "/CarlosFernandez/build/static/js/main.a5dea853.chunk.js"
+  },
+  {
+    "revision": "fa53278fa6fe35b01b28",
+    "url": "/CarlosFernandez/build/static/js/runtime~main.0e575f25.js"
+  },
+  {
+    "revision": "4d65c0433faf4d8a2597",
+    "url": "/CarlosFernandez/build/static/js/2.912afeff.chunk.js"
+  },
+  {
+    "revision": "5aef9954967125935b874aeb8042ca1d",
+    "url": "/CarlosFernandez/build/index.html"
+  }
+];
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Navbar from '../components/Navbar.js'
 import { Link } from "react-router-dom"
 
 
